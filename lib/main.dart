6fdd0a767e3a05308662d1d723223288904d7043
Calista Vimalametta <calista.vimalametta@ui.ac.id>
2024-02-25 18:37:43 +0700

    added standalone event activity feature

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,133 @@
+import 'package:flame/collisions.dart';
+import 'package:flame/components.dart';
+import 'package:flame/events.dart';
+import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(GameWidget(game: TalaCare()));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class TalaCare extends FlameGame with HasCollisionDetection, TapDetector {
+  final playerWidth = 80.0;
+  final playerHeight = 125.0;
+  final pointSize = 50.0;
+  final activitySize = 300.0;
+  late double screenWidth;
+  late double screenHeight;
+  late Activity activeActivity;
+  var isDoingActivity = false;
+  var score = 0;
 
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+  Future<void> onLoad() async {
+    super.onLoad();
+    screenWidth = size[0];
+    screenHeight = size[1];
+    add(Player(
+      Vector2(
+        screenWidth / 2 - playerWidth / 2,
+        screenHeight / 3.25 - playerHeight / 2
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      Vector2(playerWidth, playerHeight)
+    ));
+    add(Point(
+      Vector2(
+        screenWidth / 2 - pointSize / 2,
+        screenHeight / 1.25 - pointSize / 2
+      ),
+      Vector2(pointSize, pointSize)
+    ));
+  }
+
+  // activity event, currently using sprite component and tap detector
+  void onActivityTrigger() {
+    activeActivity = Activity(
+      Vector2(
+        screenWidth / 2 - activitySize / 2,
+        screenHeight / 2 - activitySize / 2
+      ),
+      Vector2(activitySize, activitySize)
     );
+    add(activeActivity);
+    isDoingActivity = true;
+    score += 1;
+  }
+
+  // for experimentation, event is set to end by tapping the screen
+  @override
+  void onTap() {
+    if (isDoingActivity) {
+      remove(activeActivity);
+      isDoingActivity = false;
+    }
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+// placeholder player, currently one variation and no animation
+class Player extends SpriteComponent with CollisionCallbacks, HasGameRef<TalaCare> {
+  var velocity = 100.0;
+  Player(position, size): super(
+    position: position,
+    size: size
+  );
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+  Future<void> onLoad() async {
+    await super.onLoad();
+    sprite = await game.loadSprite('player.png');
+    add(RectangleHitbox());
+  }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  // for experimentation, player is set to move down until it reaches activity point
+  @override
+  void update(double dt) {
+    super.update(dt);
+    y += velocity * dt;
   }
 
   @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+  void onCollision(intersectionPoints, other) {
+    super.onCollision(intersectionPoints, other);
+    velocity = 0;
+    game.onActivityTrigger();
+  }
+
+}
+
+// placeholder activity point, currently one variation
+class Point extends SpriteComponent with CollisionCallbacks, HasGameRef<TalaCare> {
+  Point(position, size): super(
+    position: position,
+    size: size
+  );
+  
+  @override
+  Future<void> onLoad() async {
+    await super.onLoad();
+    sprite = await game.loadSprite('point.png');
+    add(RectangleHitbox());
+  }
+
+  // point is set to disappear after colliding with player
+  @override
+  void onCollision(intersectionPoints, other) {
+    super.onCollision(intersectionPoints, other);
+    removeFromParent();
+  }
+
+}
+
+// placeholder activity splash art, currently one variation and no animation
+class Activity extends SpriteComponent with HasGameRef<TalaCare> {
+  Activity(position, size): super(
+    position: position,
+    size: size
+  );
+  
+  @override
+  Future<void> onLoad() async {
+    await super.onLoad();
+    sprite = await game.loadSprite('activity.png');
   }
 }
