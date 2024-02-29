2e718c8611a54bc1d8259b38e9718009d5ec1ca9
Alanna <alanna@ui.ac.id>
2024-02-29 14:03:32 +0700

    Added tests for map and collision

--- test/level_test.dart
+++ test/level_test.dart
@@ -1,21 +1,62 @@
+import 'package:flame_test/flame_test.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flame/game.dart';
-import 'package:talacare/levels/level.dart';
+import 'package:talacare/components/level.dart';
 import 'package:talacare/talacare.dart';
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
+  group('Map Loading Tests', () {
+    testWithGame<TalaCare>(
+      'Map loads when game loads', 
+      TalaCare.new,
+      (game) async {
+        await game.ready();
+        expect(game.children.query<Level>(), isNotEmpty);
+      }
+    );
+    testWithGame<TalaCare>(
+      'Default level name is level 1', 
+      TalaCare.new,
+      (game) async {
+        await game.ready();
+        expect(game.children.query<Level>().first.levelName, "Level-01");
+      }
+    );
+    testWithGame<TalaCare>(
+      'Collision blocks on map is not empty', 
+      TalaCare.new,
+      (game) async {
+        await game.ready();
+        expect(game.children.query<Level>().first.collisionBlocks, isNotEmpty);
+      }
+    );
+  });
 
-  group('Game Map Tests', () {
-    testWidgets('Map loads correctly in the game', (WidgetTester tester) async {
-      await tester.pumpWidget(GameWidget(game: TalaCare()));
+  group('Wall Collision Tests', () {
+    testWithGame<TalaCare>(
+      'Player stops at wall collision',
+      TalaCare.new,
+      (game) async {
+        await game.ready();
+        final level = game.children.query<Level>().first;
+        final player = level.player;
+        final initialPosition = player.position.clone();
+        
+        // Simulate player movement to the left
+        player.horizontalMovement = -1;
+        player.velocity.x = player.horizontalMovement * player.moveSpeed;
+        player.velocity.y = 0;
 
-      await tester.pumpAndSettle();
+        // Process movement until before left wall
+        const leftWallXPosition = 16;
+        final moveAmount = (initialPosition.x - leftWallXPosition) / player.moveSpeed;
+        game.update(moveAmount); 
 
-      final gameWidget = tester.widget<GameWidget>(find.byType(GameWidget));
-      final game = gameWidget.game as TalaCare;
-      final level = game.descendants().whereType<Level>().first;
-      expect(level.level.tileMap, isNotNull);
-    });
+        // Proceed moving, collide with left wall
+        game.update(0.2);
+        expect(player.position.x, leftWallXPosition);
+        expect(player.position.y, initialPosition.y);
+      },
+    );
   });
 }
