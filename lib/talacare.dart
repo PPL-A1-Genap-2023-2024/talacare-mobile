7dc4c12b1e1f9384e435dd8555e5f81c85fe6d0e
Alanna <alanna@ui.ac.id>
2024-02-29 14:04:24 +0700

    Added player to world

--- lib/talacare.dart
+++ lib/talacare.dart
@@ -1,26 +1,30 @@
 import 'dart:async';
 
 import 'package:flame/components.dart';
+import 'package:flame/events.dart';
 import 'package:flame/game.dart';
-import 'package:talacare/levels/level.dart';
+import 'package:flame/input.dart';
+import 'package:talacare/components/player.dart';
+import 'package:talacare/components/level.dart';
 
-class TalaCare extends FlameGame {
+class TalaCare extends FlameGame with HasKeyboardHandlerComponents {
 
   late final CameraComponent cam;
-  @override
-  final world = Level(
-    levelName: 'Level-01'
-  );
+  Player player = Player(character: 'Adam');
 
   @override
   FutureOr<void> onLoad() async {
     // Load all images into cache
     await images.loadAllImages();
 
+    final world = Level(
+      player: player,
+      levelName: 'Level-01'
+    );
+
     cam = CameraComponent.withFixedResolution(world: world, width: 360, height: 640);
     cam.viewfinder.anchor = Anchor.topLeft;
     addAll([cam, world]);
-
 
     return super.onLoad();
   }
