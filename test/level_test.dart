a351c359850e015005e7c1d63c6a0cf12f3910eb
Alanna <alanna@ui.ac.id>
2024-03-02 23:39:48 +0700

    [GREEN] Added tests when player is static and moving, from all directions

--- test/level_test.dart
+++ test/level_test.dart
@@ -1,5 +1,7 @@
+import 'package:flame/components.dart';
 import 'package:flame_test/flame_test.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:talacare/components/collision_block.dart';
 import 'package:talacare/components/level.dart';
 import 'package:talacare/helpers/directions.dart';
 import 'package:talacare/talacare.dart';
@@ -33,9 +35,9 @@ void main() {
     );
   });
 
-  group('Wall Collision Tests', () {
+  group('Dynamic Wall Collision Tests', () {
     testWithGame<TalaCare>(
-      'Player stops at wall collision',
+      'Player stops at collision from the left',
       TalaCare.new,
       (game) async {
         await game.ready();
@@ -47,34 +49,181 @@ void main() {
         player.direction = Direction.left;
 
         // Process movement until before left wall
-        const leftWallXPosition = 16;
-        final moveAmount = (initialPosition.x - leftWallXPosition) / player.moveSpeed;
+        final leftWall = player.collisionBlocks.firstWhere((wall) => wall.type == WallTypes.outerLeft);
+        final moveAmount = (initialPosition.x - leftWall.x) / player.moveSpeed;
         game.update(moveAmount); 
 
         // Proceed moving, collide with left wall
         game.update(0.2);
-        expect(player.position.x, leftWallXPosition);
-        expect(player.position.y, initialPosition.y);
+        expect(player.x, leftWall.x + leftWall.width);
+        expect(player.y, initialPosition.y);
       },
     );
+
     testWithGame<TalaCare>(
-      'Player does not move through outer walls of house',
+      'Player stops at collision from the right',
       TalaCare.new,
       (game) async {
         await game.ready();
-        // Place player next to right wall
         final level = game.children.query<Level>().first;
         final player = level.player;
-        const rightWallXPosition = 352;
-        player.position.x = rightWallXPosition-player.width;
-        // Attempt to move player through the wall
-        player.position.x += 20; // Simulate moving right into a wall
-
-        // Process the movement
-        game.update(0.1);
+        final initialPosition = player.position.clone();
         
-        // Verify player has not moved through the wall
-        expect(player.position.x, lessThanOrEqualTo(rightWallXPosition));
+        player.direction = Direction.right;
+
+        // Process movement until before right wall
+        final rightWall = player.collisionBlocks.firstWhere((wall) => wall.type == WallTypes.outerRight);
+        final moveAmount = (rightWall.x - initialPosition.x) / player.moveSpeed;
+        game.update(moveAmount); 
+
+        // Proceed moving, collide with right wall
+        game.update(0.2);
+        expect(player.x, rightWall.x - player.width);
+        expect(player.y, initialPosition.y);
+      },
+    );
+
+    testWithGame<TalaCare>(
+      'Player stops at collision from the top',
+      TalaCare.new,
+      (game) async {
+        await game.ready();
+        final level = game.children.query<Level>().first;
+        final player = level.player;
+        final initialPosition = player.position.clone();
+        
+        player.direction = Direction.up;
+
+        // Process movement until before top wall
+        final topWall = player.collisionBlocks.firstWhere((wall) => wall.type == WallTypes.outerTop);
+        final moveAmount = (initialPosition.y - topWall.y) / player.moveSpeed;
+        game.update(moveAmount); 
+
+        // Proceed moving, collide with right wall
+        game.update(0.2);
+        expect(player.y, topWall.y + topWall.height);
+        expect(player.x, initialPosition.x);
+      },
+    );
+
+    testWithGame<TalaCare>(
+      'Player stops at collision from the bottom',
+      TalaCare.new,
+      (game) async {
+        await game.ready();
+        final level = game.children.query<Level>().first;
+        final player = level.player;
+        final initialPosition = player.position.clone();
+        
+        player.direction = Direction.down;
+
+        // Process movement until before bottom wall
+        final bottomWall = player.collisionBlocks.firstWhere((wall) => wall.type == WallTypes.outerBottom);
+        final moveAmount = (bottomWall.y - initialPosition.y) / player.moveSpeed;
+        game.update(moveAmount); 
+
+        // Proceed moving, collide with bottom wall
+        game.update(0.2);
+        expect(player.y, bottomWall.y - player.height);
+        expect(player.x, initialPosition.x);
+      },
+    );
+  });
+
+  group('Static Wall Collision Tests', () {
+    testWithGame<TalaCare>(
+      'Player collides with left wall when not moving',
+      TalaCare.new,
+      (game) async {
+        await game.ready();
+        final level = game.children.query<Level>().first;
+        final player = level.player;
+        final leftWall = player.collisionBlocks.firstWhere((wall) => wall.type == WallTypes.outerLeft);
+        
+        // Place the player at the x coordinate of left wall
+        player.x = leftWall.x;
+        player.velocity = Vector2.zero();
+
+        game.update(0.1);
+
+        expect(player.position.x, greaterThanOrEqualTo(leftWall.x - leftWall.width));
+      },
+    );
+
+    testWithGame<TalaCare>(
+      'Player collides with right wall when not moving',
+      TalaCare.new,
+      (game) async {
+        await game.ready();
+        final level = game.children.query<Level>().first;
+        final player = level.player;
+        final rightWall = player.collisionBlocks.firstWhere((wall) => wall.type == WallTypes.outerRight);
+        
+        // Place the player at the x coordinate of right wall
+        player.x = rightWall.x;
+        player.velocity = Vector2.zero();
+
+        game.update(0.1);
+
+        expect(player.position.x, lessThanOrEqualTo(rightWall.x - player.width));
+      },
+    );
+
+    testWithGame<TalaCare>(
+      'Player collides with top wall when not moving',
+      TalaCare.new,
+      (game) async {
+        await game.ready();
+        final level = game.children.query<Level>().first;
+        final player = level.player;
+        final topWall = player.collisionBlocks.firstWhere((wall) => wall.type == WallTypes.outerTop);
+        
+        // Place the player at the y coordinate of top wall
+        player.y = topWall.y;
+        player.velocity = Vector2.zero();
+
+        game.update(0.1);
+
+        expect(player.position.y, greaterThanOrEqualTo(topWall.y + topWall.height));
+      },
+    );
+
+    testWithGame<TalaCare>(
+      'Player collides with bottom wall when not moving',
+      TalaCare.new,
+      (game) async {
+        await game.ready();
+        final level = game.children.query<Level>().first;
+        final player = level.player;
+        final bottomWall = player.collisionBlocks.firstWhere((wall) => wall.type == WallTypes.outerBottom);
+        
+        // Place the player at the y coordinate of bottom wall
+        player.y = bottomWall.y;
+        player.velocity = Vector2.zero();
+
+        game.update(0.1);
+
+        expect(player.position.y, lessThanOrEqualTo(bottomWall.y - player.height));
+      },
+    );
+
+    testWithGame<TalaCare>(
+      'Player collides with inner wall when not moving',
+      TalaCare.new,
+      (game) async {
+        await game.ready();
+        final level = game.children.query<Level>().first;
+        final player = level.player;
+        final initialPosition = player.position.clone();
+
+        final innerWall = player.collisionBlocks.firstWhere((wall) => wall.type == WallTypes.inside);
+       
+       // Place the player at the x and y coordinate of inner wall
+        player.position = innerWall.position;
+        player.velocity = Vector2.zero();
+        game.update(0.1);
+
+        expect(player.position, initialPosition);
       },
     );
   });
