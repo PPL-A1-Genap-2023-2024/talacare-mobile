037231ed22aaf29284169a28958f10fa34fcad86
vinsen_wijaya <36811691+vwjaya32@users.noreply.github.com>
2024-03-08 14:23:37 +0700

    [RED] feat: Movement speed loss after losing a health

--- test/health_test.dart
+++ test/health_test.dart
@@ -50,4 +50,36 @@ void main() {
     );
   });
 
+  group('Health effect to Player Movement', () {
+    testWithGame<TalaCare>(
+        'Missing Health Reduce Character Movement',
+        TalaCare.new,
+            (game) async {
+          await game.ready();
+          Hud target = game.cam.viewport.children.query<Hud>().first;
+          final initHealth = game.playerHealth;
+          final initPlayerMoveSpeed = game.player.moveSpeed;
+          target.update(target.healthDuration.toDouble());
+          expect(game.playerHealth, initHealth - 1);
+          expect(game.player.moveSpeed, isNot(initPlayerMoveSpeed));
+        }
+    );
+
+    testWithGame<TalaCare>(
+        'Player movement speed loss is 25% of current movement speed',
+        TalaCare.new,
+            (game) async {
+          await game.ready();
+          Hud target = game.cam.viewport.children.query<Hud>().first;
+          final initHealth = game.playerHealth;
+          late double lastMoveSpeed = game.player.moveSpeed;
+          for (int i = 0; i < initHealth; i++) {
+            target.update(target.healthDuration.toDouble());
+            double currentMoveSpeed = game.player.moveSpeed;
+            expect(currentMoveSpeed, lastMoveSpeed * 75/100);
+          }
+        }
+    );
+  });
+
 }
