b736526625cd2f1ee5be52a1e2672ec70f7e6091
AdrianRamadhan27 <ramadhanadrian2710@gmail.com>
2024-03-19 21:09:21 +0700

    chore: make assets for hospital and edited map

---
+++ assets/tiles/Serene Village.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Serene Village " tilewidth="16" tileheight="16" tilecount="459" columns="17">
+ <image source="../images/Serene_Village/16x16_original/Serene_Village.png" width="272" height="432"/>
+</tileset>
