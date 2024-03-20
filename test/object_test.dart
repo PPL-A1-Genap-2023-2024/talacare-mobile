80c95e1b119151980cd7e36375099d4360b9e5a0
Calista Vimalametta <calista.vimalametta@ui.ac.id>
2024-03-20 19:45:12 +0700

    test: [RED] add initial condition test for #26

---
+++ test/object_test.dart
@@ -0,0 +1,22 @@
+import 'package:flame_test/flame_test.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:talacare/talacare.dart';
+
+void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+  group('Object Selection Tests', () {
+    testWithGame<TalaCare>(
+      'Container and 3 items appear after game 2 starts (by door)', 
+      TalaCare.new,
+      (game) async {
+        await game.ready();
+        game.enterDoor();
+        await game.ready();
+        final viewport = game.cam.viewport;
+        expect(viewport.children.query<ItemContainer>().length, 1);
+        final itemContainer = viewport.children.query<ItemContainer>().first;
+        expect(itemContainer.children.query<Item>().length, 3);
+      }
+    );
+  });
+}
