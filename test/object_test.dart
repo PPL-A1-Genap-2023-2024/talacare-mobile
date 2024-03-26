d56c3f642fabbabb1b30de3e6fa0acbe6c3129fd
Calista Vimalametta <calista.vimalametta@ui.ac.id>
2024-03-26 23:00:53 +0700

    test: [RED] add silhouette matching test for #26

--- test/object_test.dart
+++ test/object_test.dart
@@ -1,23 +1,43 @@
 import 'package:flame_test/flame_test.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:talacare/components/item_container.dart';
-import 'package:talacare/components/item.dart';
+import 'package:talacare/components/draggable_container.dart';
+import 'package:talacare/components/draggable_item.dart';
+import 'package:talacare/components/silhouette_container.dart';
+import 'package:talacare/components/silhouette_item.dart';
 import 'package:talacare/talacare.dart';
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
   group('Object Selection Tests', () {
     testWithGame<TalaCare>(
-      'Container and 3 items appear after game 2 starts (by door)', 
+      '3 draggable items and 1 silhouette item appear after game 2 starts', 
       TalaCare.new,
       (game) async {
         await game.ready();
-        game.yesToHospital();
+        game.loadLevelTwoComponents();
         await game.ready();
         final viewport = game.cam.viewport;
-        expect(viewport.children.query<ItemContainer>().length, 1);
-        final itemContainer = viewport.children.query<ItemContainer>().first;
-        expect(itemContainer.children.query<Item>().length, 3);
+        expect(viewport.children.query<SilhouetteContainer>().length, 1);
+        final silhouetteContainer = viewport.children.query<SilhouetteContainer>().first;
+        expect(silhouetteContainer.children.query<SilhouetteItem>().length, 1);
+        expect(viewport.children.query<DraggableContainer>().length, 1);
+        final draggableContainer = viewport.children.query<DraggableContainer>().first;
+        expect(draggableContainer.children.query<DraggableItem>().length, 3);
+      }
+    );
+    testWithGame<TalaCare>(
+      'One of the draggable items must match with the current silhouette', 
+      TalaCare.new,
+      (game) async {
+        await game.ready();
+        game.loadLevelTwoComponents();
+        await game.ready();
+        final viewport = game.cam.viewport;
+        final silhouetteContainer = viewport.children.query<SilhouetteContainer>().first;
+        final silhouetteIndex = silhouetteContainer.indicesDisplayed.last;
+        final draggableContainer = viewport.children.query<DraggableContainer>().first;
+        final draggableIndices = draggableContainer.indicesDisplayed;
+        expect(draggableIndices.contains(silhouetteIndex), true);
       }
     );
   });
