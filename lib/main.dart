7254597901e9aaf78275632c31a32b4b02181cda
HP <fernandonathanielsutanto@gmail.com>
2024-03-26 21:53:45 +0700

    [CHORES] setup firebase authentication

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,13 @@ import 'package:flutter/material.dart';
 import 'package:talacare/talacare.dart';
 import 'package:talacare/widgets/overlays/pause_button.dart';
 import 'package:talacare/widgets/overlays/pause_menu.dart';
-
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   WidgetsFlutterBinding.ensureInitialized();
   await Flame.device.fullScreen();
   await Flame.device.setPortrait();
