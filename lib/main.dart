eb15a69da0ff40bd452ad6b2e52fb57236ea0a26
Alanna <alanna@ui.ac.id>
2024-03-27 21:52:24 +0700

    Removed unused import and whitespaces

--- lib/main.dart
+++ lib/main.dart
@@ -2,19 +2,19 @@ import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:talacare/authentication/screens/login_page.dart';
 import 'package:talacare/talacare.dart';
 import 'package:talacare/widgets/overlays/pause_button.dart';
 import 'package:talacare/widgets/overlays/pause_menu.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'authentication/firebase_options.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  
+
   await Flame.device.fullScreen();
   await Flame.device.setPortrait();
 
