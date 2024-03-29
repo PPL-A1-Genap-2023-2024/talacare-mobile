cab31df8ae48ed311cd29b47d0235fc457bf66a6
AdrianRamadhan27 <ramadhanadrian2710@gmail.com>
2024-03-29 18:17:11 +0700

    test: [RED] make test case for game transition

--- test/game_2_test.dart
+++ test/game_2_test.dart
@@ -1,4 +1,4 @@
-import 'package:flame/components.dart';
+import 'package:flame/input.dart';
 import 'package:flame_test/flame_test.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:talacare/components/game_2.dart';
@@ -10,16 +10,41 @@ import 'package:talacare/talacare.dart';
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
+  group('Game 2 Transition Test', () {
+        testWithGame<TalaCare>(
+            'Play transition when entering game 2',
+            TalaCare.new,
+                (game) async {
+            await game.ready();
+            game.currentGame = 2;
+            game.switchGame();
+            await game.ready();
+            final game_2 = game.children.query<HospitalPuzzle>().first;
+            game.update(5);
+            await game.ready();
+            final button = game_2.playButtonAnchor.children.query<SpriteButtonComponent>().first;
+            button.onTapDown(createTapDownEvents(game: game));
+            button.onTapUp(createTapUpEvents(game: game));
+            await game.ready();
+            expect(game_2.finished, true);
+            }
+        );
+  });
   group('Game 2 to 1 Integration Test', () {
     testWithGame<TalaCare>(
-        'Go back to game 1 when game 2 finish with full health',
+        'Go back to game 1 when game 2 finish with full health and progress maintained',
         TalaCare.new,
             (game) async {
           await game.ready();
+          final intersection = {Vector2(0.0,0.0), Vector2(0.0,0.0)};
           final game_1 = game.children.query<HouseAdventure>().first;
           final player = game_1.player;
+          final point = game_1.children.query<ActivityPoint>().first;
+          point.onCollision(intersection, player);
+          await game.ready();
+          final initialProgress = game.score;
           final door = game_1.children.query<HospitalDoor>().first;
-          door.onCollision({Vector2(0.0,0.0), Vector2(0.0,0.0)}, player);
+          door.onCollision(intersection, player);
           await game.ready();
           final confirmation = game.confirmation;
           expect(confirmation.reason, DialogReason.enterHospital);
@@ -30,40 +55,16 @@ void main() {
           await game.ready();
           final game_2 = game.children.query<HospitalPuzzle>().first;
           game_2.finishGame();
+          expect(game_2.finished, true);
           game.update(5);
           expect(game.currentGame, initialLevel-1);
           expect(game.playerHealth, 4);
           expect(game.player.moveSpeed, 100);
-        }
-    );
-
-    testWithGame<TalaCare>(
-        'Go back to game 1 when game 2 finish with progress maintained',
-        TalaCare.new,
-            (game) async {
-          final intersection = {Vector2(0.0,0.0), Vector2(0.0,0.0)};
-          await game.ready();
-          final game_1 = game.children.query<HouseAdventure>().first;
-          final player = game_1.player;
-          final point = game_1.children.query<ActivityPoint>().first;
-          point.onCollision(intersection, player);
-          await game.ready();
-          final initialProgress = game.score;
-          final door = game_1.children.query<HospitalDoor>().first;
-          door.onCollision({Vector2(0.0,0.0), Vector2(0.0,0.0)}, player);
-          await game.ready();
-          final confirmation = game.confirmation;
-          expect(confirmation.reason, DialogReason.enterHospital);
-          confirmation.yesButton.onTapDown(createTapDownEvents(game: game));
-          confirmation.yesButton.onTapUp(createTapUpEvents(game: game));
-          await game.ready();
-          final game_2 = game.children.query<HospitalPuzzle>().first;
-          game_2.finishGame();
-          game.update(5);
           expect(game.score, initialProgress);
 
-        }
+            }
     );
+
   });
 
 
