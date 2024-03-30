9e8dcd1f968fa149a4434789e6fff6895c4273cc
Calista Vimalametta <calista.vimalametta@ui.ac.id>
2024-03-30 17:46:49 +0700

    feat: [GREEN] add progress bar and exit button for #25 #26, fixes eff59b81

--- lib/components/game_2.dart
+++ lib/components/game_2.dart
@@ -1,19 +1,23 @@
 import 'dart:async';
-
+import 'dart:ui';
 import 'package:flame/camera.dart';
 import 'package:flame/components.dart';
+import 'package:flame/input.dart';
+import 'package:flutter/material.dart' as material;
+import 'package:talacare/components/circle_progress.dart';
 import 'package:talacare/components/draggable_container.dart';
 import 'package:talacare/components/player.dart';
 import 'package:talacare/components/silhouette_container.dart';
 import 'package:talacare/talacare.dart';
-
 import '../helpers/hospital_reason.dart';
 
 class HospitalPuzzle extends World with HasGameRef<TalaCare> {
   final HospitalReason reason;
   final Player player;
+  late final CircleProgress progressBar;
   late final DraggableContainer draggableContainer;
   late final SilhouetteContainer silhouetteContainer;
+  late final TextComponent instruction;
   late final Viewport screen;
   int score = 0;
 
@@ -21,15 +25,29 @@ class HospitalPuzzle extends World with HasGameRef<TalaCare> {
 
   @override
   FutureOr<void> onLoad() async {
-    final background = SpriteComponent();
-    background.sprite = Sprite(game.images.fromCache("Hospital/hospital_setting.jpg"));
-    background.scale = Vector2.all(0.5625);
-    gameRef.camera.backdrop = background;
+    gameRef.camera.backdrop = RectangleComponent(
+      paint: Paint()..color = Color.fromARGB(255, 243, 253, 215),
+      size: Vector2(1000, 1000)
+    );
     gameRef.camera.viewfinder.anchor = Anchor.topLeft;
     gameRef.camera.viewport = FixedAspectRatioViewport(aspectRatio: 0.5625);
 
-    final screen = gameRef.camera.viewport;
-    
+    screen = gameRef.camera.viewport;
+
+    progressBar = CircleProgress(
+      position:  Vector2(screen.size.x / 2, screen.size.y * 1 / 7),
+      width: screen.size.x,
+      totalPoints: 5
+    );
+    instruction = TextComponent(
+      anchor: Anchor.center,
+      position: Vector2(screen.size.x / 2, screen.size.y * 2 / 7),
+      text: "Ayo cocokkan gambar!",
+      textRenderer: TextPaint(style: material.TextStyle(
+        color: Color.fromARGB(255, 191, 210, 139),
+        fontSize: 28
+      ))
+    );
     silhouetteContainer = SilhouetteContainer(
       position: Vector2(screen.size.x / 2, screen.size.y / 2),
     );
@@ -37,30 +55,55 @@ class HospitalPuzzle extends World with HasGameRef<TalaCare> {
       position: Vector2(screen.size.x / 2, screen.size.y * 6 / 7),
       size: Vector2(screen.size.x, screen.size.y * 2 / 7)
     );
+    add(progressBar);
+    add(instruction);
     add(silhouetteContainer);
     add(draggableContainer);
-
-    // final newButton = SpriteButtonComponent();
-    // newButton.button = await game.loadSprite('Hospital/okay.png');
-    // newButton.buttonDown = await game.loadSprite('Hospital/okay_pressed.png');
-    // newButton.onPressed = finishGame;
-    // gameRef.camera.viewport.add(AlignComponent(
-    //   child: newButton,
-    //   alignment: Anchor.bottomCenter,
-    // ));
     return super.onLoad();
   }
 
-  void updateScore() {
+  FutureOr<void> updateScore() async {
     score++;
+    progressBar.updateProgress();
     if (score == 2) {
       draggableContainer.addSecondWaveItems();
     }
     if (score < 5) {
       silhouetteContainer.addNextItem();
     } else {
-      // exit button
+      instruction.text = "Transfusi darah berhasil!";
+      addExitButton();
     }
+  }
+
+  FutureOr<void> addExitButton() async {
+   final buttonText = TextComponent(
+      anchor: Anchor.center,
+      position: Vector2(screen.size.x / 2, screen.size.y * 6 / 7),
+      text: "Kembali ke Rumah",
+      textRenderer: TextPaint(style: material.TextStyle(
+        color: Color.fromARGB(255, 165, 151, 102),
+        fontSize: 22
+      ))
+    );
+    final button = RectangleComponent(
+      paint: Paint()..color = Color.fromARGB(255, 253, 233, 168),
+      size: Vector2(200, 50),
+    );
+    final buttonDown = RectangleComponent(
+      paint: Paint()..color = Color.fromARGB(255, 222, 202, 138),
+      size: Vector2(200, 50),
+    );
+    final buttonGroup = ButtonComponent(
+      anchor: Anchor.center,
+      button: button,
+      buttonDown: buttonDown,
+      onReleased: finishGame,
+      position: Vector2(screen.size.x / 2, screen.size.y * 6 / 7),
+      size: Vector2(200, 50),
+    );
+    add(buttonGroup);
+    add(buttonText);
   }
 
   void finishGame() {
