c35e9d7ca610698305318207806f0c1615c5c584
Alanna <alanna@ui.ac.id>
2024-04-21 18:04:28 +0700

    Added text styles and app colors

---
+++ lib/helpers/color_palette.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color greenPrimary = Color(0xFFD5EF9D);
+  static const Color purple = Color(0xFFD7A9EC);
+  static const Color textColor = Color(0xFF7F647E);
+  static const Color baseColor = Color(0xFFFFF6E6);
+  static const Color blue = Color(0xFF66AEB9);
+  static const Color coral = Color(0xFFE1827F);
+  static const Color yellow = Color(0xFFFFE57C);
+  static const Color butter = Color(0xFFFFE9AD);
+  static const Color black = Color(0xFF2E2D3C);
+}
---
+++ lib/helpers/text_styles.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:talacare/helpers/color_palette.dart';
+
+class AppTextStyles {
+  static const TextStyle h1 = TextStyle(
+    fontSize: 32,
+    fontWeight: FontWeight.w600,
+    color: AppColors.textColor,
+    fontFamily: "Fredoka One"
+  );
+
+  static const TextStyle h2 = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w600,
+    color: AppColors.textColor,
+    fontFamily: "Fredoka One"
+  );
+
+  static const TextStyle largeBold = TextStyle(
+    fontSize: 22,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Fredoka',
+    color: AppColors.textColor
+  );
+
+}
