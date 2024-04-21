aa62eb149223bceb2bc3e08212af9c5af0ad71f5
Falensi Azmi <falensiazmi@gmail.com>
2024-04-21 22:09:06 +0700

    [RED] add test for navigate to export data page

--- test/export_data_test.dart
+++ test/export_data_test.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:talacare/export_data/screens/export_page.dart';
 import 'package:talacare/widgets/homepage.dart';
+import 'package:mockito/mockito.dart';
+
+class MockNavigatorObserver extends Mock implements NavigatorObserver {}
 
 void main() {
   testWidgets('Export Data Button Existence Test', (WidgetTester tester) async {
@@ -10,5 +14,20 @@ void main() {
     GlobalKey exportButtonKey = home.getExportButtonKey();
     final exportButtonFinder = find.byKey(exportButtonKey);
     expect(exportButtonFinder, findsOneWidget);
+  });
+  testWidgets('Navigate To Export Data Test', (WidgetTester tester) async {
+    HomePage home = HomePage();
+    final mockObserver = MockNavigatorObserver();
+    await tester.pumpWidget(MaterialApp(
+      home: home,
+      navigatorObservers: [mockObserver],
+    ));
+    await tester.pump();
+    GlobalKey exportButtonKey = home.getExportButtonKey();
+    final exportButtonFinder = find.byKey(exportButtonKey);
+    expect(find.byType(ExportPage), findsNothing);
+    await tester.tap(exportButtonFinder);
+    await tester.pumpAndSettle();
+    expect(find.byType(ExportPage), findsOne);
   });
 }
