aa680a78d7591bc8193637204fc76e235e5e5bec
Falensi Azmi <falensiazmi@gmail.com>
2024-04-21 23:26:57 +0700

    [GREEN] implement export page (not working yet)

--- lib/export_data/screens/export_page.dart
+++ lib/export_data/screens/export_page.dart
@@ -1,15 +1,74 @@
 import 'package:flutter/material.dart';
 
 class ExportPage extends StatefulWidget {
-  const ExportPage({Key? key}) : super(key: key);
+  ExportPage({Key? key}) : super(key: key);
+
+  final GlobalKey _backButtonKey = GlobalKey();
+  final GlobalKey _downloadButtonKey = GlobalKey();
+
+  GlobalKey getBackButtonKey() {
+    return _backButtonKey;
+  }
+
+  GlobalKey getDownloadButtonKey() {
+    return _downloadButtonKey;
+  }
 
   @override
-  State<ExportPage> createState() => _ExportPageState();
+  State<ExportPage> createState() => _ExportPageState(
+      backButtonKey: _backButtonKey, downloadButtonKey: _downloadButtonKey);
 }
 
 class _ExportPageState extends State<ExportPage> {
+  final GlobalKey backButtonKey;
+  final GlobalKey downloadButtonKey;
+  _ExportPageState(
+      {required this.backButtonKey, required this.downloadButtonKey});
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Mengunduh Data Pemain',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            Container(
+                child: Column(
+              children: [
+                Text(
+                  'Tuliskan nama file yang akan diunduh',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      labelText: 'talacare_data',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+                ElevatedButton(
+                  key: downloadButtonKey,
+                  onPressed: () {},
+                  child: Text('Unduh'),
+                ),
+                ElevatedButton(
+                  key: backButtonKey,
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text('Kembali'),
+                ),
+              ],
+            )),
+          ],
+        ),
+      ),
+    );
   }
 }
