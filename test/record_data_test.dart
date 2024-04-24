32110df17e6b438df3c7fa24aa3f94d2163c5634
falensiazmi <falensiazmi@gmail.com>
2024-04-24 09:22:05 +0700

    [RED] add test for time recording"

---
+++ test/record_data_test.dart
@@ -0,0 +1,17 @@
+import 'package:flame_test/flame_test.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:talacare/talacare.dart';
+
+void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+  group('Timestamp Functionality Test', () {
+    testWithGame<TalaCare>('Check Start and End Timestamp', TalaCare.new,
+        (game) async {
+      await game.ready();
+      game.update(1000);
+      game.victory();
+      expect(game.endTimestamp.difference(game.startTimestamp),
+          greaterThan(Duration(seconds: 0)));
+    });
+  });
+}
