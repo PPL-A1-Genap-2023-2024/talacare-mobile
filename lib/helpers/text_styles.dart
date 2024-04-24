3ead659e9d15e30f1ce0bb7259fe5724543a069a
Alanna <alanna@ui.ac.id>
2024-04-24 10:35:02 +0700

    Add more text styles

--- lib/helpers/text_styles.dart
+++ lib/helpers/text_styles.dart
@@ -3,30 +3,54 @@ import 'package:talacare/helpers/color_palette.dart';
 
 class AppTextStyles {
   static const TextStyle h1 = TextStyle(
-    fontSize: 32,
-    fontWeight: FontWeight.w600,
-    color: AppColors.textColor,
-    fontFamily: "Fredoka One"
-  );
+      fontSize: 32,
+      fontWeight: FontWeight.w600,
+      color: AppColors.textColor,
+      fontFamily: "Fredoka One");
 
   static const TextStyle h2 = TextStyle(
-    fontSize: 24,
-    fontWeight: FontWeight.w600,
-    color: AppColors.textColor,
-    fontFamily: "Fredoka One"
-  );
+      fontSize: 24,
+      fontWeight: FontWeight.w600,
+      color: AppColors.textColor,
+      fontFamily: "Fredoka One");
 
-  static const TextStyle largeBold = TextStyle(
-    fontSize: 22,
-    fontWeight: FontWeight.w400,
-    fontFamily: 'Fredoka',
-    color: AppColors.textColor
-  );
-
-  static const TextStyle normalText = TextStyle(
+  static const TextStyle normal = TextStyle(
     color: AppColors.textColor,
     fontSize: 16,
     fontFamily: 'Fredoka',
     fontWeight: FontWeight.w500,
   );
+
+  static const TextStyle normalBold = TextStyle(
+    color: AppColors.textColor,
+    fontSize: 16,
+    fontFamily: 'Fredoka',
+    fontWeight: FontWeight.w600,
+  );
+
+  static const TextStyle medium = TextStyle(
+    color: AppColors.textColor,
+    fontSize: 18,
+    fontFamily: 'Fredoka',
+    fontWeight: FontWeight.w500,
+  );
+
+  static const TextStyle mediumBold = TextStyle(
+    color: AppColors.textColor,
+    fontSize: 18,
+    fontFamily: 'Fredoka',
+    fontWeight: FontWeight.w600,
+  );
+
+  static const TextStyle large = TextStyle(
+      fontSize: 22,
+      fontWeight: FontWeight.w500,
+      fontFamily: 'Fredoka',
+      color: AppColors.textColor);
+
+  static const TextStyle largeBold = TextStyle(
+      fontSize: 22,
+      fontWeight: FontWeight.w600,
+      fontFamily: 'Fredoka',
+      color: AppColors.textColor);
 }
