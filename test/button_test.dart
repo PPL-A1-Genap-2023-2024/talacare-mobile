0e6998777e137d8f973f05b9c2ca1d0c002b9794
Alanna <alanna@ui.ac.id>
2024-04-27 23:31:50 +0700

    [RED] Test for custom button component

---
+++ test/button_test.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:talacare/components/button.dart';
+
+void main() {
+  testWidgets('CustomButton renders correctly', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Scaffold(
+          body: CustomButton(
+            text: 'Test Button',
+            onPressed: () {},
+            initialButtonWidth: 500,
+          ),
+        ),
+      ),
+    );
+
+    expect(find.text('Test Button'), findsOneWidget);
+
+    await tester.tap(find.byType(InkWell));
+
+    expect(tester.getSize(find.byType(CustomButton)).width, 500);
+  });
+
+  testWidgets('CustomButton with icon renders correctly', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Scaffold(
+          body: CustomButton(
+            text: 'Test Button',
+            onPressed: () {},
+            assetImagePath: 'assets/images/Illustrations/google.png',
+          ),
+        ),
+      ),
+    );
+
+    expect(find.byKey(Key('icon_container')), findsOneWidget);  
+  });
+
+  testWidgets('CustomButton with empty text', (WidgetTester tester) async {
+     await tester.pumpWidget(
+      MaterialApp(
+        home: Scaffold(
+          body: CustomButton(
+            text: 'Test Button',
+            onPressed: () {},
+            initialButtonWidth: -100, // Negative buttonWidth
+          ),
+        ),
+      ),
+    );
+    // Verify that the button has a minimum width
+    expect(tester.getSize(find.byType(CustomButton)).width, CustomButton.defaultButtonWidth);
+  });
+}
