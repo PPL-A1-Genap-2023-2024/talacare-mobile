2a8631c65b0bd414742859b2a9938263fa34ded2
Falensi Azmi <falensiazmi@gmail.com>
2024-05-02 00:20:42 +0700

    [RED] Add test for handle game paused

--- test/record_data_test.dart
+++ test/record_data_test.dart
@@ -1,4 +1,5 @@
 import 'package:flame_test/flame_test.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:talacare/talacare.dart';
 
@@ -10,8 +11,20 @@ void main() {
       await game.ready();
       game.update(1000);
       game.victory();
-      expect(DateTime.now().difference(game.startTimestamp),
-          greaterThan(Duration(seconds: 0)));
+      expect(game.totalTime, greaterThan(0));
+    });
+    testWithGame<TalaCare>('Check Time When The Game is Paused', TalaCare.new,
+        (game) async {
+      await game.ready();
+      game.lifecycleStateChange(AppLifecycleState.paused);
+      game.lifecycleStateChange(AppLifecycleState.hidden);
+      game.update(1000);
+      expect(game.paused, true);
+      int timeNow = game.totalTime;
+      game.update(1000);
+      expect(game.totalTime, timeNow);
+      game.lifecycleStateChange(AppLifecycleState.resumed);
+      expect(game.paused, false);
     });
   });
 }
