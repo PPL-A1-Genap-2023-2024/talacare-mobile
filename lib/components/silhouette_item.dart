d473387327de6dc0734706dc74d2d32d6e425f6b
Calista Vimalametta <calista.vimalametta@ui.ac.id>
2024-05-01 19:02:43 +0700

    refactor: increase smaller silhouette items' hitbox, #45

--- lib/components/silhouette_item.dart
+++ lib/components/silhouette_item.dart
@@ -23,7 +23,10 @@ class SilhouetteItem extends SpriteComponent
     position = Vector2(item.x, item.y);
     sprite = Sprite(game.images.fromCache('Game_2/item_${item.name}.png'));
     tint(Color.fromARGB(255, 255, 255, 255));
-    hitbox = RectangleHitbox(size: Vector2(item.x / 2, item.y / 2));
+    hitbox = RectangleHitbox();
+    if (size.x < 50) {
+      hitbox.scale = Vector2.all(2);
+    }
     add(hitbox);
     return super.onLoad();
   }
