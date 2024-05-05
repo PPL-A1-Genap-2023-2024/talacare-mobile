d847653909f88e46a58922f9823620bd8277694e
HP <fernandonathanielsutanto@gmail.com>
2024-05-06 00:42:37 +0700

    Add untracked files

---
+++ lib/helpers/color_palette.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color greenPrimary = Color(0xFFD5EF9D);
+  static const Color purple = Color(0xFFD7A9EC);
+  static const Color plum = Color(0xFF745573);
+  static const Color baseColor = Color(0xFFFFF6E6);
+  static const Color blue = Color(0xFF66AEB9);
+  static const Color coral = Color(0xFFE1827F);
+  static const Color yellow = Color(0xFFFFE57C);
+  static const Color butter = Color(0xFFFFE9AD);
+  static const Color black = Color(0xFF2E2D3C);
+  static const Color textColor = plum;
+}
