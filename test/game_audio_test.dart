230b8edb488d8a4b6fb0929f979df40bef0cb5ec
Alanna <alanna@ui.ac.id>
2024-05-07 21:00:47 +0700

    [RED] Implement flame audio

---
+++ test/game_audio_test.dart
@@ -0,0 +1,14 @@
+import 'package:flame_audio/flame_audio.dart';
+import 'package:flame_test/flame_test.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:talacare/talacare.dart';
+
+void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+  group('Game audio tests', () {
+    testWithGame<TalaCare>('Background music should play in game', TalaCare.new,
+        (game) async {
+      await game.ready();
+    });
+  });
+}
