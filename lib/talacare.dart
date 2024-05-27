fa3bf71eec696d29372e5a657f35779d509648f8
vinsen_wijaya <36811691+vwjaya32@users.noreply.github.com>
2024-05-27 20:50:00 +0700

    fix: testing problem

--- lib/talacare.dart
+++ lib/talacare.dart
@@ -42,7 +42,6 @@ class TalaCare extends FlameGame
   @override
   late World world;
   late Minigame minigame;
-  late AlignComponent eventAnchor;
   late AlignComponent confirmationAnchor;
   bool eventIsActive = false;
   bool confirmationIsActive = false;
@@ -188,28 +187,6 @@ class TalaCare extends FlameGame
   void changeDirection(Direction direction) {
     player.direction = direction;
   }
-
-  // Future<void> onActivityStart(ActivityPoint point) async {
-  //   if (!eventIsActive) {
-  //     world.remove(point);
-  //     eventAnchor = AlignComponent(
-  //         child: ActivityEvent(variant: point.variant),
-  //         alignment: Anchor.center);
-  //     // Set Priority
-  //     eventAnchor.priority = 10;
-  //     camera.viewport.add(eventAnchor);
-  //     eventIsActive = true;
-  //     score += 1;
-  //   }
-  // }
-  //
-  // void onActivityEnd(ActivityEvent event) {
-  //   if (eventIsActive) {
-  //     eventAnchor.remove(event);
-  //     camera.viewport.remove(eventAnchor);
-  //     eventIsActive = false;
-  //   }
-  // }
 
   void enableDarkBackground() {
     gameOne.hud.timerStarted = false;
